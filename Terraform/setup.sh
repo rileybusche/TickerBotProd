b4a9b936c4eed196dd0c054c968c8f15a2e33a3a
rileybusche <riley.busche@gmail.com>
2019-07-23 11:30:41 -0500

    Removed a line

--- Terraform/setup.sh
+++ Terraform/setup.sh
@@ -3,7 +3,6 @@ sudo apt-get update
 sudo apt install python3-pip -y
 python3 -m pip install -U discord.py
 python3 -m pip install apscheduler
-
 # Git clone
 cd /home/ubuntu/
 git clone https://github.com/rileybusche/TickerBotProd.git
