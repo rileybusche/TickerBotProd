7a1ea2c5ff715b8263af6b1288b6411f84ac9a86
rileybusche <riley.busche@gmail.com>
2019-07-23 10:24:17 -0500

    Changed ip output

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,10 +4,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "Ticker" {
-  count         = 1
-  ami           = "ami-026c8acd92718196b"
-  instance_type = "t2.micro"
-  key_name      = "${aws_key_pair.ticker_alert_key.key_name}"
+  count                       = 1
+  ami                         = "ami-026c8acd92718196b"
+  instance_type               = "t2.micro"
+  key_name                    = "${aws_key_pair.ticker_alert_key.key_name}"
+  associate_public_ip_address = true
 
   user_data     = "${file("setup.sh")}"
 
