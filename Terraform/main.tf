38d26ffc8fc40bc989eb3b09a50967da5a3ce902
rileybusche <riley.busche@gmail.com>
2019-07-23 10:48:15 -0500

    Added new format for user data

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,14 +3,18 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+data "template_file" "user_data" {
+  template = "${file("setup.sh")}"
+}
+
 resource "aws_instance" "Ticker" {
-  count                       = 1
+  count                       = 0
   ami                         = "ami-026c8acd92718196b"
   instance_type               = "t2.micro"
   key_name                    = "${aws_key_pair.ticker_alert_key.key_name}"
   associate_public_ip_address = true
 
-  user_data     = "${file("setup.sh")}"
+  user_data     = "${data.template_file.user_data.rendered}"
 
   tags = {
     Name = "TickerAlert"
