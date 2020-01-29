2b12a61b06a989e84375274cc1444d2c96752ecd
rileybusche <riley.busche@gmail.com>
2020-01-28 19:07:44 -0600

    Added in bot_quit

--- TickerPriceBot/api_helper.py
+++ TickerPriceBot/api_helper.py
@@ -4,7 +4,19 @@ import json
 
 api_key = open('api_key.txt', 'r').read().strip()
 
+def stock_price(ticker):
+    call = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={api_key}'
+    response = requests.get(call)
+    json_response = response.json()
+    print(json_response)
+
+    return json_response
+
 def crypto_price(ticker):
-    try:
-        response = requests.get(f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={api_key}')
-        json_response = response.json()
+    call = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={ticker}&to_currency=USD&apikey={api_key}'
+    response = requests.get(call)
+    
+    json_response = response.json()
+    print(json_response)
+
+    return json_response
