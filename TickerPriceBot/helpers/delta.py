408fb0f3585fdb5743b8b608b4353872b9a5dd22
rileybusche <riley.busche@gmail.com>
2020-03-16 12:58:09 -0500

    Testing

--- TickerPriceBot/helpers/delta.py
+++ TickerPriceBot/helpers/delta.py
@@ -4,13 +4,20 @@ import datetime
 def ticker_delta(price_data):
     date = datetime.datetime.now()
 
-    market_time_open = f'{date.year}-{date.month}-{date.day} 09:31:00'
-
     hour = date.hour + 1
+    day = date.day
+    month = date.month
+
     if hour < 10:
         hour = f'0{str(hour)}'
+    if day < 10:
+        day = f'0{str(day)}'
+    if month < 10:
+        month = f'0{str(month)}'
 
-    market_time_now = f'{date.year}-{date.month}-{date.day} {hour}:{date.minute}:00'
+    
+    market_time_open = f'{date.year}-{month}-{day} 09:31:00'
+    market_time_now = f'{date.year}-{month}-{day} {hour}:{date.minute}:00'
 
     market_price_open = float(price_data[market_time_open]['1. open'])
     market_price_now = float(price_data[market_time_now]['4. close'])
